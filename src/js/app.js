4553453edfd129cdba693475908cf5e5b6cf60f7
odinokun <odinokun@gmail.com>
2020-12-26 14:21:31 +0200

    'commit'

---
+++ src/js/app.js
@@ -0,0 +1,32 @@
+$(function() {
+
+  // begin SVG for IE
+  require('./modules/svg4everybody')();
+
+  // begin no-JS
+  require('./modules/noJs')();
+
+  // begin language
+  require('./modules/lang')();
+
+  // begin header search field
+  require('./modules/header-search')();
+
+  // begin phone mask
+  // require('./modules/maskedinput')();
+
+  // begin Scroll2Ancor
+  // require('./modules/scroll2ancor')();
+
+  // begin Slick slider
+  // require('./modules/slick')();
+
+  // begin Popup
+  // require('./modules/popup')();
+
+  // begin Animate.css
+  //РАБОТАЕТ ТОЛЬКО С JQUERY < 3.0.0 !!!!!!!!!!!!!!!!!!
+  // require('./modules/waypoints')();
+  // require('./modules/animateCss')();
+
+});
